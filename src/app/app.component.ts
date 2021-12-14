29e29fb586f0d002a12b721a73e4e07af70b25bc
Sergey <ssotnikov1990@gmail.com>
2021-12-14 22:14:02 +0300

    date/time

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,27 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  styleUrls: ['./app.component.sass'],
 })
 export class AppComponent {
+  today = new Date();
+  DD = String(this.today.getDate()).padStart(2, '0');
+  MM = String(this.today.getMonth() + 1).padStart(2, '0'); //January is 0!
+  yyyy = String(this.today.getFullYear()).padStart(2, '0');
+  hh = String(this.today.getHours()).padStart(2, '0');
+  mm = String(this.today.getMinutes()).padStart(2, '0');
+  ss = String(this.today.getSeconds()).padStart(2, '0');
 
+  currentDay =
+    this.DD +
+    '.' +
+    this.MM +
+    '.' +
+    this.yyyy +
+    ' ' +
+    this.hh +
+    ':' +
+    this.mm +
+    ':' +
+    this.ss;
 }
