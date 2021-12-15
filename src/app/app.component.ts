cdb20999b3696e64fd3e74bbc5a0d29a414df587
Sergey <ssotnikov1990@gmail.com>
2021-12-15 22:30:43 +0300

    complite first and second task

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,31 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  styleUrls: ['./app.component.sass'],
 })
 export class AppComponent {
 
+  firstTask() {
+    const date1 = new Date('12.6.21');
+    const date2 = new Date('12.10.21');
+    const differenceTime = date2.getTime() - date1.getTime();
+    const differenceTimeDays = Math.ceil(differenceTime / (1000 * 3600 * 24));
+    
+    console.log(date1);
+    console.log(date2);
+    console.log(differenceTimeDays);
+  }
+
+  secondTask(){
+    const today = new Date();
+    const currentDay = today.getDay();
+    const difference = today.getDate() - currentDay + (currentDay == 0 ? -6:1); 
+    const monday =  new Date(today.setDate(difference)); 
+    console.log(monday);
+  }
+
+  ngOnInit() {
+    this.firstTask();
+    this.secondTask();
+  }
 }
